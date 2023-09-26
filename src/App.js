afe143d6236553a1129f9987b0c7de33e7459b69
Chrotesque <github@chrotesque.com>
2023-09-26 11:24:48 +0000

    Add infinite scroll & search func to task listing

--- src/App.js
+++ src/App.js
@@ -7,18 +7,32 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import TaskCreateForm from "./pages/tasks/TaskCreateForm";
 import TaskPage from "./pages/tasks/TaskPage";
+import TasksPage from "./pages/tasks/TasksPage";
+import { useCurrentUser } from "./context/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <TasksPage
+                message="No tasks found. Time to create a few!"
+                filter={`owner__profile=${profile_id}&`}
+              />
+            )}
+          />
           <Route exact path="/login" render={() => <SignInForm />} />
           <Route exact path="/register" render={() => <SignUpForm />} />
-          <Route exact path="/tasks/create" render={() => <TaskCreateForm />} />
-          <Route exact path="/tasks/:id" render={() => <TaskPage />} />
+          <Route exact path="/task/create" render={() => <TaskCreateForm />} />
+          <Route exact path="/task/:id" render={() => <TaskPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
