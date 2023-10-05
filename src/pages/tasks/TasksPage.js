0ce059b3db38ef71c6734f254543ba92ddb2b760
Chrotesque <github@chrotesque.com>
2023-10-05 14:21:38 +0000

    Fix to #27
    Changing useState for const [tasks, setTasks] and length/map access @ line 69/70 to accomodate for the changed API structure without pagination

--- src/pages/tasks/TasksPage.js
+++ src/pages/tasks/TasksPage.js
@@ -14,13 +14,12 @@ import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
-import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import { useCurrentUser } from "../../context/CurrentUserContext";
 
 function TasksPage({ message, filter = "" }) {
   const currentUser = useCurrentUser();
-  const [tasks, setTasks] = useState({ results: [] });
+  const [tasks, setTasks] = useState([]);
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
@@ -33,7 +32,7 @@ function TasksPage({ message, filter = "" }) {
         setTasks(data);
         setHasLoaded(true);
       } catch (err) {
-        console.log(err);
+        console.log("fetchTasks Errors:" + err);
       }
     };
 
@@ -50,8 +49,7 @@ function TasksPage({ message, filter = "" }) {
   const taskListing = (
     <>
       <Row className="h-100">
-        <Col className="py-2 p-0 p-lg-2" lg={8}>
-          <p>Popular profiles mobile</p>
+        <Col className="py-2 p-0 p-lg-2" lg={12}>
           <i className={`fas fa-search ${styles.SearchIcon}`} />
           <Form
             className={styles.SearchBar}
@@ -65,33 +63,27 @@ function TasksPage({ message, filter = "" }) {
               placeholder="Search tasks"
             />
           </Form>
-
           {hasLoaded ? (
             <>
-              {tasks.results.length ? (
-                <InfiniteScroll
-                  children={tasks.results.map((task) => (
-                    <Task key={task.id} {...task} setTasks={setTasks} />
-                  ))}
-                  dataLength={tasks.results.length}
-                  loader={<Asset spinner />}
-                  hasMore={!!tasks.next}
-                  next={() => fetchMoreData(tasks, setTasks)}
-                />
-              ) : (
-                <Container className={appStyles.Content}>
-                  <Asset src={NoResults} message={message} />
-                </Container>
-              )}
+              <Row>
+                {tasks.length ? (
+                  tasks.map((task) => (
+                    <Col lg={3}>
+                      <Task key={task.id} {...task} setTasks={setTasks} />
+                    </Col>
+                  ))
+                ) : (
+                  <Container className={appStyles.Content}>
+                    <Asset src={NoResults} message={message} />
+                  </Container>
+                )}
+              </Row>
             </>
           ) : (
             <Container className={appStyles.Content}>
               <Asset spinner />
             </Container>
           )}
-        </Col>
-        <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-          <p>Popular profiles for desktop</p>
         </Col>
       </Row>
     </>
